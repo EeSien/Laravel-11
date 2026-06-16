3c4e9a18c01cb004bd46ba9876cfe430bb39e341
EeSien <esthien@hotmail.com>
2026-06-16 10:37:54 +0800

    update the wrong error message

--- app/Helpers/Response.php
+++ app/Helpers/Response.php
@@ -15,7 +15,7 @@ public function success($data = null, string $message = 'Success', int $status =
         ], $status);
     }
 
-public function error($data = null, string $message = 'Success', int $status = 422): JsonResponse
+public function error($data = null, string $message = 'Error', int $status = 422): JsonResponse
     {
         return response()->json([
             'message' => $message,
